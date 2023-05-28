e799f830b4cfc5cfc05dbee35f4f9b2944726974
rustam-1108d <133682982+rustam-1108d@users.noreply.github.com>
2023-05-28 16:48:41 +0600

    step 1

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ import _ from 'lodash';
 
 export default function solution(content) {
   // BEGIN
-  console.log(content);
+  // console.log(content);
+  const rows = content.split('\n');
+  // console.log(rows);
+
+  const headlines = rows.slice(0, 1);
+  // console.log(headlines);
+
+  const data = rows.slice(1, -1);
+  // console.log(data);
+
+  const dataLinesCount = `Count: ${data.length}`
+  console.log(dataLinesCount)
   // END
 }
