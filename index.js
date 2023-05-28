7d302e0ab66dc420d81ed266b56937ee4565e74b
rustam-1108d <133682982+rustam-1108d@users.noreply.github.com>
2023-05-28 18:03:28 +0600

    steps 2 and 3

--- index.js
+++ index.js
@@ -5,16 +5,71 @@ import _ from 'lodash';
 export default function solution(content) {
   // BEGIN
   // console.log(content);
+
+  // step 1
   const rows = content.split('\n');
   // console.log(rows);
 
-  const headlines = rows.slice(0, 1);
+  const headlinesRow = rows.slice(0, 1);
   // console.log(headlines);
 
-  const data = rows.slice(1, -1);
+  const dataRows = rows.slice(1, -1);
   // console.log(data);
 
-  const dataLinesCount = `Count: ${data.length}`
-  console.log(dataLinesCount)
+  const dataLinesCount = `Count: ${dataRows.length}`;
+  console.log(dataLinesCount);
+
+  // step 2
+
+  const splitRows = (row) => row.split(',');
+
+  // DATA TO USE
+  const dataCells = dataRows.map(splitRows);
+  // console.log(dataCells);
+  // console.log(dataCells[0][7]);
+
+  const extractCity = (row) => row[7];
+  const citiesColumn = dataCells.map(extractCity);
+  // console.log(citiesColumn);
+
+  const uniqueCities = [];
+  for (const city of citiesColumn) {
+    if (!uniqueCities.includes(city)) {
+      uniqueCities.push(city);
+    }
+  }
+  // console.log(uniqueCities);
+
+  const uniqueCitiesSorted = uniqueCities.sort();
+  console.log(`Cities: ${uniqueCitiesSorted.join(', ')}`);
+
+  // step 3
+  // console.log(dataCells[0][3]);
+  const extractHumidity = (row) => row[3];
+  const humidityColumn = dataCells.map(extractHumidity);
+  // console.log(humidityColumn);
+
+  let minHumidity = humidityColumn[0];
+  // console.log(minHumidity);
+  for (const humidity of humidityColumn) {
+    if (humidity < minHumidity) {
+      minHumidity = humidity;
+    }
+  }
+  // console.log(minHumidity);
+
+  let maxHumidity = humidityColumn[0];
+  // console.log(maxHumidity);
+  for (const humidity of humidityColumn) {
+    if (humidity > maxHumidity) {
+      maxHumidity = humidity;
+    }
+  }
+  // console.log(maxHumidity);
+
+  console.log(`Humidity: Min: ${minHumidity}, Max: ${maxHumidity}`);
+
+  // step 4
+
   // END
 }
